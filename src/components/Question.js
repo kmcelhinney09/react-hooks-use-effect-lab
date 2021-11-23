15cc2a793ec285f7bafd2cadf0f5e6a2df8045e0
mcelhinneyk <mr.mcelhinney@gmail.com>
2021-11-23 15:17:10 -0800

    commit tree 0feda7e3e831f6a85822f5f2f060f1e2bfaf72f6

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timerID = setTimeout(() => {
+      if (timeRemaining > 0) {
+        setTimeRemaining((timeRemaining) => timeRemaining - 1);
+      } else {
+        onAnswered(false);
+        setTimeRemaining(10);
+      }
+
+    }, 1000);
+
+    return function cleanup() {
+      clearTimeout(timerID);
+    }
+  })
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
